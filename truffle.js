89640255d16d7d4380f67f95cf220180a99b354a
Marc Queiroz <marc.queiroz@gmail.com>
2019-07-02 14:19:32 -0300

    Compiler and packages adjustments

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
 module.exports = {
+  compilers: {
+    solc: {
+      version: "0.4.16",
+    },
+  },
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      gas: 500000,
+      gas: 7600000,
+      gasPrice: 200000000,
       network_id: "*" // Match any network id
     }
   }
